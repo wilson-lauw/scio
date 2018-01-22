20691bda526f97858bdbdbb0df0980423ef8d6d6
Neville Li <neville@spotify.com>
2018-01-22 12:45:14 -0500

    x

---
+++ scio-avro/src/test/scala/com/spotify/scio/avro/types/DebugTest.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.avro.types
+
+import org.scalatest._
+class DebugTest extends FlatSpec with Matchers {
+
+  @AvroType.fromSchema(
+    """
+      |{
+      |  "name": "A", "namespace": "outer",
+      |  "type": "record", "fields": [
+      |    {
+      |      "name": "a",
+      |      "type": {
+      |        "type": "record",
+      |        "name": "A", "namespace": "inner",
+      |        "fields": [{"name": "intF", "type": "int"}]
+      |      }
+      |    }
+      |  ]
+      |}
+    """.stripMargin)
+  class SameName
+
+  it should "support nested record with same name as enclosing record" in {
+    val r = SameName(SameName$A(2))
+    SameName.toGenericRecord(r).getSchema.toString
+  }
+
+}
